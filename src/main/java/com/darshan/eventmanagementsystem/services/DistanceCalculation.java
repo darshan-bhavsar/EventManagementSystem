a41acd2a21e57cb4dd35b5b4911078de96fd6323
Darshan bhavsar <darshanbhavsar19@gmail.com>
2024-04-09 00:23:20 +0530

    added distance api

--- src/main/java/com/darshan/eventmanagementsystem/services/DistanceCalculation.java
+++ src/main/java/com/darshan/eventmanagementsystem/services/DistanceCalculation.java
@@ -12,17 +12,16 @@ public class DistanceCalculation
     private RestTemplate restTemplate;
     private Event event;
     public Double getDistance(Double latitude1,Double longitude1,Double latitude2,Double longitude2){
-
         RestTemplate restTemplate = new RestTemplate();
+        Event e = new Event();
         String url = "https://gg-backend-assignment.azurewebsites.net/api/Distance?code=IAKvV2EvJa6Z6dEIUqqd7yGAu7IZ8gaH-a0QO6btjRc1AzFu8Y3IcQ==&latitude1=" + latitude1 + "&longitude1=" + longitude1 + "&latitude2=" + latitude2 + "&longitude2=" + longitude2;
 
-        Double response= restTemplate.getForObject(url, Double.class);
-        if(response == null){
+        DistanceResponse response = restTemplate.getForObject(url, DistanceResponse.class);
+
+        if(response.getDistance() == null){
             System.out.println("Distance not found");
             return 0.0;
         }
-
-        return response;
-//"https://gg-backend-assignment.azurewebsites.net/api/Distance?code=IAKvV2EvJa6Z6dEIUqqd7yGAu7IZ8gaH-a0QO6btjRc1AzFu8Y3IcQ==&latitude1=40.7128&longitude1=-74.0060&latitude2=25.5169968004073&longitude2=-173.22570039222800"
+        return Double.parseDouble(response.getDistance());
     }
 }
---
+++ src/main/java/com/darshan/eventmanagementsystem/services/DistanceResponse.java
@@ -0,0 +1,14 @@
+package com.darshan.eventmanagementsystem.services;
+
+public class DistanceResponse {
+    private String distance;
+
+    // getters and setters
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+}
